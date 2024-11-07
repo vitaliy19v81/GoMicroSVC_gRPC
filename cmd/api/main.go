efeb50ccdc28376436f3a2b522f45c724b5fadc9
Vitaliy <Alex_halesterin@mail.ru>
2024-11-07 17:38:19 +0300

    изменена функция createKafkaTopic

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -42,7 +42,7 @@ func main() {
 	topic := cfg.KafkaTopic
 
 	// Вызов функции создания топика
-	err = CreateKafkaTopic(brokers[0], topic, 1, 1)
+	err = CreateKafkaTopic(brokers, topic, 1, 1) // brokers[0]
 	if err != nil {
 		log.Fatalf("Ошибка создания топика: %v\n", err)
 	}
@@ -100,29 +100,92 @@ func main() {
 	log.Println("Сервер успешно завершен.")
 }
 
-// CreateKafkaTopic Функция для создания топика в Kafka
-func CreateKafkaTopic(brokerAddress, topic string, numPartitions, replicationFactor int) error {
-	log.Printf("Подключение к брокеру Kafka: %s\n", brokerAddress)
+//// CreateKafkaTopic Функция для создания топика в Kafka
+//func CreateKafkaTopic(brokerAddress, topic string, numPartitions, replicationFactor int) error {
+//	log.Printf("Подключение к брокеру Kafka: %s\n", brokerAddress)
+//
+//	// Устанавливаем соединение с брокером Kafka
+//	conn, err := kafka.Dial("tcp", brokerAddress)
+//	if err != nil {
+//		return fmt.Errorf("ошибка подключения к Kafka: %v", err)
+//	}
+//	defer func() {
+//		if err := conn.Close(); err != nil {
+//			log.Printf("Ошибка закрытия соединения с брокером: %v\n", err)
+//		}
+//	}()
+//
+//	// Получаем информацию о контроллере кластера
+//	controller, err := conn.Controller()
+//	if err != nil {
+//		return fmt.Errorf("ошибка получения контроллера Kafka: %v", err)
+//	}
+//	log.Printf("Контроллер Kafka: %s:%d\n", controller.Host, controller.Port)
+//
+//	// Устанавливаем соединение с контроллером
+//	controllerConn, err := kafka.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
+//	if err != nil {
+//		return fmt.Errorf("ошибка подключения к контроллеру Kafka: %v", err)
+//	}
+//	defer func() {
+//		if err := controllerConn.Close(); err != nil {
+//			log.Printf("Ошибка закрытия соединения с контроллером: %v\n", err)
+//		}
+//	}()
+//
+//	// Конфигурация нового топика
+//	topicConfigs := []kafka.TopicConfig{
+//		{
+//			Topic:             topic,
+//			NumPartitions:     numPartitions,
+//			ReplicationFactor: replicationFactor,
+//		},
+//	}
+//
+//	// Создаем топик
+//	err = controllerConn.CreateTopics(topicConfigs...)
+//	if err != nil {
+//		return fmt.Errorf("ошибка создания топика: %v", err)
+//	}
+//
+//	log.Printf("Топик успешно создан: %s\n", topic)
+//	return nil
+//}
 
-	// Устанавливаем соединение с брокером Kafka
-	conn, err := kafka.Dial("tcp", brokerAddress)
-	if err != nil {
-		return fmt.Errorf("ошибка подключения к Kafka: %v", err)
-	}
-	defer func() {
-		if err := conn.Close(); err != nil {
-			log.Printf("Ошибка закрытия соединения с брокером: %v\n", err)
+// CreateKafkaTopic создает новый топик в Kafka
+func CreateKafkaTopic(brokers []string, topic string, numPartitions, replicationFactor int) error {
+	var conn *kafka.Conn
+	var err error
+
+	// Пытаемся подключиться к каждому брокеру из списка
+	for _, brokerAddress := range brokers {
+		conn, err = kafka.Dial("tcp", brokerAddress)
+		if err == nil {
+			log.Printf("Успешное подключение к брокеру Kafka: %s\n", brokerAddress)
+			defer func() {
+				if err := conn.Close(); err != nil {
+					log.Printf("Ошибка закрытия соединения с брокером: %v\n", err)
+				}
+			}()
+			break
+		} else {
+			log.Printf("Не удалось подключиться к брокеру Kafka: %s, пробуем следующий", brokerAddress)
 		}
-	}()
+	}
 
-	// Получаем информацию о контроллере кластера
+	// Если не удалось подключиться ни к одному брокеру
+	if conn == nil {
+		return fmt.Errorf("не удалось подключиться ни к одному из брокеров Kafka: %v", brokers)
+	}
+
+	// Получаем контроллер кластера
 	controller, err := conn.Controller()
 	if err != nil {
 		return fmt.Errorf("ошибка получения контроллера Kafka: %v", err)
 	}
 	log.Printf("Контроллер Kafka: %s:%d\n", controller.Host, controller.Port)
 
-	// Устанавливаем соединение с контроллером
+	// Подключаемся к контроллеру для создания топика
 	controllerConn, err := kafka.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
 	if err != nil {
 		return fmt.Errorf("ошибка подключения к контроллеру Kafka: %v", err)
@@ -133,7 +196,7 @@ func CreateKafkaTopic(brokerAddress, topic string, numPartitions, replicationFac
 		}
 	}()
 
-	// Конфигурация нового топика
+	// Конфигурация для создания нового топика
 	topicConfigs := []kafka.TopicConfig{
 		{
 			Topic:             topic,
